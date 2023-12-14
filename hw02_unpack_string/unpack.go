41c114618fc4fc5c89e37754da6137ec54afc802
Elberion <58712867+Elberion@users.noreply.github.com>
2023-12-14 18:26:57 +1000

    Strings.Builder replaced by string()

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,11 +10,9 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
 	var prevStr, curStr string
-	builder := strings.Builder{}
 	resultStr := strings.Builder{}
 	for _, v := range str {
-		builder.WriteRune(v)
-		curStr = builder.String()
+		curStr = string(v)
 		if i, err := strconv.Atoi(curStr); err == nil {
 			if prevStr != "" {
 				resultStr.WriteString(strings.Repeat(prevStr, i))
@@ -26,7 +24,6 @@ func Unpack(str string) (string, error) {
 			resultStr.WriteString(prevStr)
 			prevStr = curStr
 		}
-		builder.Reset()
 	}
 	if _, err := strconv.Atoi(curStr); err != nil {
 		resultStr.WriteString(curStr)
